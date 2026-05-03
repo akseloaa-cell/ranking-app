b8e05780f96211f08233efd6eb9a23946ee452ba
akseloaa-cell <akseloaa@gmail.com>
2026-05-03 13:38:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { nextMatch } from "./match.js";
 import { update } from "./ranking.js";
-
+import { showStats, closeStats } from "./stats.js";
 import * as ui from "./ui.js";
 import * as stats from "./stats.js";
 import * as tournament from "./tournament.js";
@@ -11,6 +11,8 @@ window.showStats = stats.showStats;
 window.closeStats = stats.closeStats;
 window.startTournament = tournament.startTournament;
 window.scrollToTop = ui.scrollToTop;
+window.showStats = showStats;
+window.closeStats = closeStats;
 
 // start
 update();
