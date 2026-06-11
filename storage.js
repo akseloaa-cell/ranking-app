317b1159411b7d38c9ff3de4b9a497508993d676
akseloaa-cell <akseloaa@gmail.com>
2026-06-11 14:13:19 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,13 +1,10 @@
 import { state } from "./state.js";
 
-export function save(){
-  localStorage.setItem("items", JSON.stringify(state.items));
-  localStorage.setItem("categories", JSON.stringify(state.categories));
-  localStorage.setItem("recentMatches", JSON.stringify(state.recentMatches));
-  localStorage.setItem("dailyTournament", JSON.stringify(state.tournament));
-  localStorage.setItem("previousRanking", JSON.stringify(state.previousRanking));
-  localStorage.setItem("previousRankingByCategory", JSON.stringify(state.previousRankingByCategory));
-  localStorage.setItem("lastRankingDate", state.lastRankingDate);
+function save() {
+  localStorage.setItem("rankingApp", JSON.stringify({
+    ...state,
+    version: STATE_VERSION
+  }));
 }
 
 function load() {
