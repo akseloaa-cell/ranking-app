f3b67f5d6f8a8eab161355dfcd6d7a8f1067d102
akseloaa-cell <akseloaa@gmail.com>
2026-06-14 00:38:24 +0200

    Update tournament.js

--- tournament.js
+++ tournament.js
@@ -1,5 +1,13 @@
 import { state } from "./state.js";
 
-export function startTournament(){
-  console.log("Tournament starter");
+export function startTournament() {
+  state.mode = "tournament";
+
+  document.getElementById("homeView").style.display = "none";
+
+  document.getElementById("categorySelectView").style.display = "none";
+
+  document.getElementById("tournamentSection").style.display = "block";
+
+  console.log("Tournament åpnet");
 }
